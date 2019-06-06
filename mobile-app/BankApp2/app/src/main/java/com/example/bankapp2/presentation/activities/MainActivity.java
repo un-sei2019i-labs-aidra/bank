3be2c1020d7e6cec9dee27809cfe90de7228836e
Diana Guarin <dcguarina@unal.edu.co>
2019-06-06 07:17:35 -0500

    Package bussinesLogic and Presentation created

---
+++ mobile-app/BankApp2/app/src/main/java/com/example/bankapp2/presentation/activities/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.bankapp2.presentation.activities;
+
+public class MainActivity {
+
+
+}
---
+++ mobile-app/BankApp2/app/src/main/java/com/example/bankapp2/presentation/activities/SendMoneyActivity.java
@@ -0,0 +1,4 @@
+package com.example.bankapp2.presentation.activities;
+
+public class SendMoneyActivity {
+}
